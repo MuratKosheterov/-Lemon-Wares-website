4bb93da72e44e38fe22a0d069b671ad879357079
Mura <kmx31525@gmail.com>
2023-10-01 13:32:01 +0500

    lem

---
+++ settings.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function () {
+    $('.price__list').slick({
+        dots: true,
+        infinite: true,
+        speed: 700,
+        // autoplay: true,
+        slidesToShow: 3,
+        // slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 481,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+});
